82ba0badd6d134aabdbafc922030597bbedfef93
malevitzch <malevitzch@gmail.com>
2024-11-25 10:51:49 +0100

    Added function for checking if the backend is reachable

--- app/frontend/page/js/index.js
+++ app/frontend/page/js/index.js
@@ -1,5 +1,11 @@
-function display() {
-  fetch('/api/')
-  .then(response => response.text())
-  .then(data => console.log(data));
+function establish_host() {
+  return fetch('/api/handshake')
+  .then(response => {
+    return response.ok;
+  })
+  .catch(error => {
+    return false;
+  });
 }
+
+export { establish_host };
